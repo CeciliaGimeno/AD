dd56f81a360b6a0c34198e2c86d5173f0f6678fc
CeciliaGimeno <ceciliagimeno1@gmail.com>
2014-09-19 10:15:59 +0200

    19

---
+++ PHolaMySql/PHolaMySql/Menu.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace PHolaMySql
+{
+	public class Menu
+	{
+		public Menu ()
+		{
+			Console.WriteLine ("seleccione un opción:");
+			Console.WriteLine ("_____________________");
+			Console.WriteLine ("0.Salir");
+			Console.WriteLine ("1.Nuevo");
+			Console.WriteLine ("2.Modifificar");
+			Console.WriteLine ("3.Eliminar");
+			Console.WriteLine ("4.Ver");
+
+			int opcion=Console.ReadLine();
+
+			switch(opcion) {
+				case 0:
+					Console.WriteLine("Hasta pronto!!");
+					break;
+				case 1:
+					Console.WriteLine("The number is zero!");
+					break;
+				case 2:
+					Console.WriteLine("The number is zero!");
+					break;
+				case 3:
+					Console.WriteLine("The number is zero!");
+					break;
+				case 4:
+					Console.WriteLine("The number is zero!");
+					break;
+			}
+
+		}
+	}
+}
+
--- PHolaMySql/PHolaMySql/Program.cs
+++ PHolaMySql/PHolaMySql/Program.cs
@@ -7,23 +7,29 @@ namespace PHolaMySql
 	{
 		public static void Main (string[] args)
 		{
-
+			//Conexión con su constructor y su cadena de conexión
 			MySqlConnection mySqlConnection = new MySqlConnection (
 				"Data Source=localhost;Database=dbprueba;User ID=root;Password=sistemas");
+			//abrir la conexión
 			mySqlConnection.Open ();
 			Console.WriteLine ("Hello World!");
-			/*MySqlCommand mySqlCommand = mySqlConnection.CreateCommand ();
+			/*//Para insertar en la bbdd
+			 MySqlCommand mySqlCommand = mySqlConnection.CreateCommand ();
 			mySqlCommand.CommandText=
 				string.Format("Insert into categoria(nombre) values ('{0}')", 
 				              DateTime.Now);
 			//Si el dato lo introduce el usuario puede dar problemas de seguridad.
 
-			//para ejecutar comandos sencillos, nos devuelve un entero, que son las filas afectadas.
+			//para ejecutar comandos sencillos como insert, nos devuelve un entero, que son las filas afectadas.
 			mySqlCommand.ExecuteNonQuery();*/
 
+			//para ejecutar comandos como select de varias filas y columnas, con create comand que nos devuelve un mysqlcommand
 			MySqlCommand mySqlCommand = mySqlConnection.CreateCommand ();
 			mySqlCommand.CommandText = "select * from categoria";
+
+			//mySqlCommand.ExecuteReader nos devuelve un mysqlsdatareader 
 			MySqlDataReader mySqlDataReader = mySqlCommand.ExecuteReader ();
+
 
 			Console.WriteLine ("FieldCount={0}",mySqlDataReader.FieldCount);
 			//sustituye directamente {0} por lo que se indica tras la coma.
@@ -31,8 +37,19 @@ namespace PHolaMySql
 			for (int index=0; index< mySqlDataReader.FieldCount; index ++)
 				Console.WriteLine ("column {0}={1}", index, mySqlDataReader.GetName(index));
 
-			mySqlConnection.Close ();
+			// para acceder a cada fila hace falta Read() 
+			while (mySqlDataReader.Read()) {
+				//lo guarda en un object para evitar castings
+				object id= mySqlDataReader ["id"];
+				object nombre= mySqlDataReader ["nombre"];
+				Console.WriteLine ("id={0} Nombre={1}", id, nombre);
 
+			}
+			//hay que cerrar los datareader, al terminar o para abrir otro pues no puede haber dos abiertos.
+			mySqlDataReader.Close ();
+
+			//cerramos la conexión
+			mySqlConnection.Close ();
 
 		}
 	}
