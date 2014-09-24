e7e52c29c41de45e517f7192256dca64f4f0a444
CeciliaGimeno <ceciliagimeno1@gmail.com>
2014-09-24 20:55:13 +0200

    menu

--- PHolaMySql/PHolaMySql/Menu.cs
+++ PHolaMySql/PHolaMySql/Menu.cs
@@ -1,12 +1,18 @@
 using System;
+using MySql.Data.MySqlClient;
 
 namespace PHolaMySql
 {
 	public class Menu
 	{
+		int opcion,id;
+		string nombre,nuevo;
+
 		public Menu ()
 		{
-			Console.WriteLine ("seleccione un opción:");
+			do {
+			//System.Console.Clear(); 
+			Console.WriteLine ("Seleccione un opción:");
 			Console.WriteLine ("_____________________");
 			Console.WriteLine ("0.Salir");
 			Console.WriteLine ("1.Nuevo");
@@ -14,25 +20,41 @@ namespace PHolaMySql
 			Console.WriteLine ("3.Eliminar");
 			Console.WriteLine ("4.Ver");
 
-			//int opcion=Console.ReadLine();
+			opcion=int.Parse(Console.ReadLine());
 
 			switch(opcion) {
 				case 0:
 					Console.WriteLine("Hasta pronto!!");
 					break;
 				case 1:
-					Console.WriteLine("The number is zero!");
+					Console.WriteLine("Nuevo registro");
+					Console.WriteLine("Introduce nombre");
+					nuevo=Console.ReadLine();
+					MySqlConnection mySqlConnection = new MySqlConnection ("Data Source=localhost;Database=dbprueba;User ID=root;Password=sistemas");
+					//abrir la conexión
+					mySqlConnection.Open ();
+					MySqlCommand mySqlCommand = mySqlConnection.CreateCommand ();
+					mySqlCommand.CommandText= string.Format("Insert into categoria(nombre) values ('{0}')", nuevo);
+					mySqlCommand.ExecuteNonQuery();
+					mySqlConnection.Close ();
 					break;
 				case 2:
-					Console.WriteLine("The number is zero!");
+					Console.WriteLine("Modificar registro");
+					Console.WriteLine("Introduce nombre");
+					nombre=Console.ReadLine();
+					Console.WriteLine("Introduce id");
+					id=int.Parse(Console.ReadLine());
 					break;
 				case 3:
-					Console.WriteLine("The number is zero!");
+					Console.WriteLine("Eliminar registro");
+					Console.WriteLine("Introduce id");
+					id=int.Parse(Console.ReadLine());
 					break;
 				case 4:
-					Console.WriteLine("The number is zero!");
+					Console.WriteLine("Ver registros");
 					break;
 			}
+			} while (opcion!=0);
 
 		}
 	}
